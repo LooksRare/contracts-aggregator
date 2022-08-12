f28ea8d47849499f27f459282dba21adca5fa82d
hiroshi <hiroshi@looksrare.org>
2022-08-12 10:48:03 +0100

    fix: Compile warnings

--- test/typescript/utils/get-fixture.ts
+++ test/typescript/utils/get-fixture.ts
@@ -1,5 +1,6 @@
 import * as fs from "fs";
 import * as path from "path";
 
-export default (filename: string) =>
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default (filename: string): any =>
   JSON.parse(fs.readFileSync(path.join(__dirname, `../fixtures/${filename}`), { encoding: "utf8", flag: "r" }));
