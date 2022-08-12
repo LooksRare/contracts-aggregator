8ecb83f88cbe069fa28c6d522bc5a7ce52810510
hiroshi <hiroshi@looksrare.org>
2022-08-12 10:45:48 +0100

    chore: Add interface for ABI

--- test/typescript/utils/get-abi.ts
+++ test/typescript/utils/get-abi.ts
@@ -1,5 +1,6 @@
 import * as fs from "fs";
 import * as path from "path";
+import { AbiFunction } from "../interfaces/abi";
 
-export default (filename: string) =>
+export default (filename: string): AbiFunction[] =>
   JSON.parse(fs.readFileSync(path.join(__dirname, `../../../abis/${filename}`), { encoding: "utf8", flag: "r" }));
