147390135a621dd3a91af36b64f46e5e8e2add0b
hiroshi <hiroshi@looksrare.org>
2022-08-17 21:59:17 +0100

    test: LooksRareAggregator unit tests

---
+++ test/foundry/LooksRareAggregator.t.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.14;
+
+import {LooksRareAggregator} from "../../contracts/LooksRareAggregator.sol";
+import {LooksRareProxy} from "../../contracts/proxies/LooksRareProxy.sol";
+import {OwnableTwoSteps} from "@looksrare/contracts-libs/contracts/OwnableTwoSteps.sol";
+import {TestHelpers} from "./TestHelpers.sol";
+
+abstract contract TestParameters {
+    address internal _notOwner = address(1);
+}
+
+contract LooksRareAggregatorTest is TestParameters, TestHelpers {
+    LooksRareAggregator aggregator;
+    LooksRareProxy looksRareProxy;
+
+    function setUp() public {
+        aggregator = new LooksRareAggregator();
+        looksRareProxy = new LooksRareProxy();
+    }
+
+    function testAddFunction() public {
+        assertTrue(!aggregator.supportsProxyFunction(address(looksRareProxy), LooksRareProxy.buyWithETH.selector));
+        vm.expectEmit(true, true, false, true);
+        emit LooksRareAggregator.FunctionAdded(address(looksRareProxy), LooksRareProxy.buyWithETH.selector);
+        aggregator.addFunction(address(looksRareProxy), LooksRareProxy.buyWithETH.selector);
+        assertTrue(aggregator.supportsProxyFunction(address(looksRareProxy), LooksRareProxy.buyWithETH.selector));
+    }
+
+    function testAddFunctionNotOwner() public {
+        vm.prank(_notOwner);
+        vm.expectRevert(OwnableTwoSteps.NotOwner.selector);
+        aggregator.addFunction(address(looksRareProxy), LooksRareProxy.buyWithETH.selector);
+    }
+
+    function testRemoveFunction() public {
+        assertTrue(!aggregator.supportsProxyFunction(address(looksRareProxy), LooksRareProxy.buyWithETH.selector));
+        aggregator.addFunction(address(looksRareProxy), LooksRareProxy.buyWithETH.selector);
+        assertTrue(aggregator.supportsProxyFunction(address(looksRareProxy), LooksRareProxy.buyWithETH.selector));
+
+        vm.expectEmit(true, true, false, true);
+        emit LooksRareAggregator.FunctionRemoved(address(looksRareProxy), LooksRareProxy.buyWithETH.selector);
+        aggregator.removeFunction(address(looksRareProxy), LooksRareProxy.buyWithETH.selector);
+        assertTrue(!aggregator.supportsProxyFunction(address(looksRareProxy), LooksRareProxy.buyWithETH.selector));
+    }
+
+    function testRemoveFunctionNotOwner() public {
+        aggregator.addFunction(address(looksRareProxy), LooksRareProxy.buyWithETH.selector);
+
+        vm.prank(_notOwner);
+        vm.expectRevert(OwnableTwoSteps.NotOwner.selector);
+        aggregator.removeFunction(address(looksRareProxy), LooksRareProxy.buyWithETH.selector);
+    }
+}
