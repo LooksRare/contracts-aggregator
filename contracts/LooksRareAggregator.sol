73fda232fce2505782235a28ce6a936e6d9e12b9
hiroshi <hiroshi@looksrare.org>
2022-08-17 13:32:00 +0100

    docs: LooksRareAggregator Natspec

--- contracts/LooksRareAggregator.sol
+++ contracts/LooksRareAggregator.sol
@@ -14,23 +14,39 @@ import {LowLevelETH} from "./lowLevelCallers/LowLevelETH.sol";
  */
 contract LooksRareAggregator is OwnableTwoSteps, LowLevelETH {
     struct TradeData {
-        address proxy;
-        bytes4 selector;
-        uint256 value;
-        BasicOrder[] orders;
-        bytes[] ordersExtraData;
-        bytes extraData;
+        address proxy; // The marketplace proxy's address
+        bytes4 selector; // The marketplace proxy's function selector
+        uint256 value; // The amount of ETH passed to the proxy during the function call
+        BasicOrder[] orders; // An array of orders to be executed by the marketplace
+        bytes[] ordersExtraData; // Extra data per order, specific for each marketplace
+        bytes extraData; // Extra data specific for each marketplace
     }
 
     mapping(address => mapping(bytes4 => bool)) private proxyFunctionSelectors;
 
+    /// @notice Emitted when a marketplace proxy's function is enabled.
+    /// @param proxy The marketplace proxy's address
+    /// @param selector The marketplace proxy's function selector
     event FunctionAdded(address indexed proxy, bytes4 selector);
+
+    /// @notice Emitted when a marketplace proxy's function is disabled.
+    /// @param proxy The marketplace proxy's address
+    /// @param selector The marketplace proxy's function selector
     event FunctionRemoved(address indexed proxy, bytes4 selector);
+
+    /// @notice Emitted when buyWithETH is complete
+    /// @param sweeper The address that submitted the transaction
+    /// @param tradeCount Total trade count
+    /// @param successCount Successful trade count (if only 1 out of N trades in
+    ///                     an order succeeds, it is consider successful)
     event Sweep(address indexed sweeper, uint256 tradeCount, uint256 successCount);
 
     error InvalidFunction();
     error TradeExecutionFailed();
 
+    /// @notice Execute NFT sweeps in different marketplaces in a single transaction
+    /// @param tradeData Data object to be passed downstream to each marketplace's proxy for execution
+    /// @param isAtomic Flag to enable atomic trades (all or nothing) or partial trades
     function buyWithETH(TradeData[] calldata tradeData, bool isAtomic) external payable {
         uint256 successCount;
         for (uint256 i; i < tradeData.length; ) {
@@ -74,16 +90,27 @@ contract LooksRareAggregator is OwnableTwoSteps, LowLevelETH {
         emit Sweep(msg.sender, tradeData.length, successCount);
     }
 
+    /// @notice Enable calling the specified proxy's trade function
+    /// @dev Must be called by the current owner
+    /// @param proxy The marketplace proxy's address
+    /// @param selector The marketplace proxy's function selector
     function addFunction(address proxy, bytes4 selector) external onlyOwner {
         proxyFunctionSelectors[proxy][selector] = true;
         emit FunctionAdded(proxy, selector);
     }
 
+    /// @notice Disable calling the specified proxy's trade function
+    /// @dev Must be called by the current owner
+    /// @param proxy The marketplace proxy's address
+    /// @param selector The marketplace proxy's function selector
     function removeFunction(address proxy, bytes4 selector) external onlyOwner {
         delete proxyFunctionSelectors[proxy][selector];
         emit FunctionRemoved(proxy, selector);
     }
 
+    /// @param proxy The marketplace proxy's address
+    /// @param selector The marketplace proxy's function selector
+    /// @return Whether the marketplace proxy's function can be called from the aggregator
     function supportsProxyFunction(address proxy, bytes4 selector) external view returns (bool) {
         return proxyFunctionSelectors[proxy][selector];
     }
