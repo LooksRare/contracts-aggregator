73697b5ad471f764d433dd7cf007734c3491d834
hiroshi <hiroshi@looksrare.org>
2022-08-17 15:02:29 +0100

    docs: Document BasicOrder parameters

--- contracts/libraries/OrderStructs.sol
+++ contracts/libraries/OrderStructs.sol
@@ -4,15 +4,15 @@ pragma solidity 0.8.14;
 import {CollectionType} from "./OrderEnums.sol";
 
 struct BasicOrder {
-  address signer; // signer for LooksRare, offerer for Seaport
-  address recipient;
-  address collection;
-  CollectionType collectionType;
-  uint256[] tokenIds;
-  uint256[] amounts; // always 1 when ERC-721, can be > 1 if ERC-1155
-  uint256 price;
-  address currency;
-  uint256 startTime;
-  uint256 endTime;
+  address signer; // The order's maker
+  address recipient; // Recipient of the token purchased, it can be different from the address that submitted the transaction
+  address collection; // The address of the ERC-721/ERC-1155 token to be purchased
+  CollectionType collectionType; // 0 for ERC-721, 1 for ERC-1155
+  uint256[] tokenIds; // The IDs of the tokens to be purchased
+  uint256[] amounts; // Always 1 when ERC-721, can be > 1 if ERC-1155
+  uint256 price; // The price to pay for the order
+  address currency; // The order's currency, address(0) for ETH
+  uint256 startTime; // The timestamp when the order starts becoming valid
+  uint256 endTime; // The timestamp when the order stops becoming valid
   bytes signature; // split to v,r,s for LooksRare
 }
