6dc7d3fc65ccba67c214291369c91a31e247373c
hiroshi <hiroshi@looksrare.org>
2022-08-22 14:14:04 +0100

    docs: Add missing Natspec

--- contracts/TokenRescuer.sol
+++ contracts/TokenRescuer.sol
@@ -12,10 +12,21 @@ import {LowLevelERC20} from "./lowLevelCallers/LowLevelERC20.sol";
  * @author LooksRare protocol team (👀,💎)
  */
 contract TokenRescuer is OwnableTwoSteps, LowLevelETH, LowLevelERC20 {
+    /**
+     * @notice Rescue the contract's trapped ETH
+     * @dev Must be called by the current owner
+     * @param to Send the contract's ETH balance to this address
+     */
     function rescueETH(address to) external onlyOwner {
         _transferETH(to, address(this).balance);
     }
 
+    /**
+     * @notice Rescue any of the contract's trapped ERC-20 tokens
+     * @dev Must be called by the current owner
+     * @param currency The address of the ERC-20 token to rescue from the contract
+     * @param to Send the contract's specified ERC-20 token balance to this address
+     */
     function rescueERC20(address currency, address to) external onlyOwner {
         uint256 amount = IERC20(currency).balanceOf(address(this));
         _executeERC20DirectTransfer(currency, to, amount);
