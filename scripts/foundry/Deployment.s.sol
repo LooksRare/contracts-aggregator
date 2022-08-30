29bff6680b7bdf60cd2ff0721b9ad6cb63077ec0
hiroshi <hiroshi@looksrare.org>
2022-08-30 17:16:25 +0100

    build: Deployment script

---
+++ scripts/foundry/Deployment.s.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.14;
+
+import {Script} from "forge-std/Script.sol";
+import {LooksRareAggregator} from "../contracts/LooksRareAggregator.sol";
+import {LooksRareProxy} from "../contracts/proxies/LooksRareProxy.sol";
+import {SeaportProxy} from "../contracts/proxies/SeaportProxy.sol";
+import {X2Y2Proxy} from "../contracts/proxies/X2Y2Proxy.sol";
+import {CryptoPunksProxy} from "../contracts/proxies/CryptoPunksProxy.sol";
+import {SudoswapProxy} from "../contracts/proxies/SudoswapProxy.sol";
+
+contract Deployment is Script {
+    LooksRareAggregator looksRareAggregator;
+    LooksRareProxy looksRareProxy;
+    SeaportProxy seaportProxy;
+    X2Y2Proxy x2y2Proxy;
+    CryptoPunksProxy cryptoPunksProxy;
+    SudoswapProxy sudoswapProxy;
+
+    function run() public {
+        vm.startBroadcast();
+
+        looksRareAggregator = new LooksRareAggregator();
+
+        looksRareProxy = new LooksRareProxy();
+        looksRareAggregator.addFunction(address(looksRareProxy), LooksRareProxy.buyWithETH.selector);
+
+        seaportProxy = new SeaportProxy();
+        looksRareAggregator.addFunction(address(seaportProxy), SeaportProxy.buyWithETH.selector);
+
+        x2y2Proxy = new X2Y2Proxy();
+        looksRareAggregator.addFunction(address(x2y2Proxy), X2Y2Proxy.buyWithETH.selector);
+
+        cryptoPunksProxy = new CryptoPunksProxy();
+        looksRareAggregator.addFunction(address(cryptoPunksProxy), CryptoPunksProxy.buyWithETH.selector);
+
+        sudoswapProxy = new SudoswapProxy();
+        looksRareAggregator.addFunction(address(sudoswapProxy), SudoswapProxy.buyWithETH.selector);
+
+        vm.stopBroadcast();
+    }
+}
