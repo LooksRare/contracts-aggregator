5c64c47147702b7b379e46599afef2c5a6a5069e
0xhiroshi <108636367+0xhiroshi@users.noreply.github.com>
2022-09-01 11:08:19 +0100

    feat: Sudoswap integration (#9)

---
+++ contracts/interfaces/ISudoswapPair.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.14;
+
+contract CurveErrorCodes {
+    enum Error {
+        OK, // No error
+        INVALID_NUMITEMS, // The numItem value is 0
+        SPOT_PRICE_OVERFLOW // The updated spot price doesn't fit into 128 bits
+    }
+}
+
+interface ISudoswapPair {
+    function getBuyNFTQuote(uint256 numItems)
+        external
+        view
+        returns (
+            CurveErrorCodes.Error error,
+            uint256 newSpotPrice,
+            uint256 newDelta,
+            uint256 inputAmount,
+            uint256 protocolFee
+        );
+}
