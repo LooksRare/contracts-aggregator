e9a6154edf9bcaf26b726c80bbf6814350ab8a93
hiroshi <hiroshi@looksrare.org>
2022-09-01 14:28:56 +0100

    test: ERC-20 refund test

--- test/typescript/shared-tests/behaves-like-seaport-multiple-currencies.ts
+++ test/typescript/shared-tests/behaves-like-seaport-multiple-currencies.ts
@@ -67,5 +67,55 @@ export default function behavesLikeSeaportMultipleCurrencies(isAtomic: boolean):
     expect(await bayc.balanceOf(buyer.address)).to.equal(2);
     expect(await bayc.ownerOf(9996)).to.equal(buyer.address);
     expect(await bayc.ownerOf(5509)).to.equal(buyer.address);
+
+    expect(await usdc.balanceOf(buyer.address)).to.equal(0);
+    expect(await usdc.allowance(buyer.address, aggregator.address)).to.equal(0);
+  });
+
+  it("Should be able to refund extra ERC-20 tokens paid", async function () {
+    const { aggregator, buyer, proxy, functionSelector, bayc } = await loadFixture(deploySeaportFixture);
+
+    const orderOne = getFixture("seaport", "bayc-9996-order.json");
+    const orderTwo = getFixture("seaport", "bayc-5509-order.json");
+
+    // priceOne is in USDC and priceTwo is in ETH
+    const priceOne = combineConsiderationAmount(orderOne.parameters.consideration);
+    const priceTwo = combineConsiderationAmount(orderTwo.parameters.consideration);
+
+    // Doesn't look good here but the buyer is the first address, meaning the contract deployer/owner
+    await proxy.connect(buyer).approve(USDC);
+
+    const excess = ethers.utils.parseUnits("100", 6);
+
+    await airdropUSDC(buyer.address, priceOne.add(excess));
+
+    const usdc = await ethers.getContractAt("IERC20", USDC);
+    await usdc.connect(buyer).approve(aggregator.address, priceOne.add(excess));
+
+    const tradeData = [
+      {
+        tokenTransfers: [{ amount: priceOne.add(excess), currency: USDC }],
+        proxy: proxy.address,
+        selector: functionSelector,
+        value: priceTwo,
+        orders: [getSeaportOrderJson(orderOne, priceOne), getSeaportOrderJson(orderTwo, priceTwo)],
+        ordersExtraData: [getSeaportOrderExtraData(orderOne), getSeaportOrderExtraData(orderTwo)],
+        extraData: isAtomic ? encodedExtraData() : ethers.constants.HashZero,
+      },
+    ];
+
+    const tx = await aggregator.connect(buyer).execute(tradeData, buyer.address, isAtomic, { value: priceTwo });
+    const receipt = await tx.wait();
+
+    validateSweepEvent(receipt, buyer.address, 1, 1);
+
+    expect(await bayc.balanceOf(buyer.address)).to.equal(2);
+    expect(await bayc.ownerOf(9996)).to.equal(buyer.address);
+    expect(await bayc.ownerOf(5509)).to.equal(buyer.address);
+
+    expect(await usdc.balanceOf(aggregator.address)).to.equal(0);
+    expect(await usdc.balanceOf(proxy.address)).to.equal(0);
+    expect(await usdc.balanceOf(buyer.address)).to.equal(excess);
+    expect(await usdc.allowance(buyer.address, aggregator.address)).to.equal(0);
   });
 }
