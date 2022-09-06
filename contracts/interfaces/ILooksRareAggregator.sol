b90c56431a626826e211382cc498d2630b0bda62
hiroshi <hiroshi@looksrare.org>
2022-09-06 11:19:36 +0100

    chore: Remove UnauthorizedToPullTokens

--- contracts/interfaces/ILooksRareAggregator.sol
+++ contracts/interfaces/ILooksRareAggregator.sol
@@ -41,6 +41,5 @@ interface ILooksRareAggregator {
     error InvalidFunction();
     error InvalidOrderLength();
     error TradeExecutionFailed();
-    error UnauthorizedToPullTokens();
     error ZeroAddress();
 }
