e460c92b81d56dd4c4c20a57ab61897f03f2ce7b
hiroshi <hiroshi@looksrare.org>
2022-09-09 12:08:42 +0100

    build: Update deployment script

--- scripts/foundry/Deployment.s.sol
+++ scripts/foundry/Deployment.s.sol
@@ -22,20 +22,21 @@ contract Deployment is Script {
 
         looksRareAggregator = new LooksRareAggregator();
 
-        looksRareProxy = new LooksRareProxy();
+        looksRareProxy = new LooksRareProxy(0x59728544B08AB483533076417FbBB2fD0B17CE3a);
         looksRareAggregator.addFunction(address(looksRareProxy), LooksRareProxy.execute.selector);
 
-        seaportProxy = new SeaportProxy();
+        seaportProxy = new SeaportProxy(0x00000000006c3852cbEf3e08E8dF289169EdE581);
         looksRareAggregator.addFunction(address(seaportProxy), SeaportProxy.execute.selector);
 
-        x2y2Proxy = new X2Y2Proxy();
+        x2y2Proxy = new X2Y2Proxy(0x74312363e45DCaBA76c59ec49a7Aa8A65a67EeD3);
         looksRareAggregator.addFunction(address(x2y2Proxy), X2Y2Proxy.execute.selector);
 
-        cryptoPunksProxy = new CryptoPunksProxy();
+        cryptoPunksProxy = new CryptoPunksProxy(0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB);
         looksRareAggregator.addFunction(address(cryptoPunksProxy), CryptoPunksProxy.execute.selector);
 
-        sudoswapProxy = new SudoswapProxy();
-        looksRareAggregator.addFunction(address(sudoswapProxy), SudoswapProxy.execute.selector);
+        // TODO: Wait until Sudoswap router V2 is available
+        // sudoswapProxy = new SudoswapProxy(0x2B2e8cDA09bBA9660dCA5cB6233787738Ad68329);
+        // looksRareAggregator.addFunction(address(sudoswapProxy), SudoswapProxy.execute.selector);
 
         vm.stopBroadcast();
     }
