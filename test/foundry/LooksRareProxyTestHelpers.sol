590d2cf82c2a4d1f2d2e6030182cd534aee5c2ce
0xhiroshi <108636367+0xhiroshi@users.noreply.github.com>
2022-09-14 09:53:50 +0100

    perf: GemSwap benchmark (#34)
    * test: GemSwapBenchmarkTest
    
    * test: GemSwapBenchmarkTest 2 NFTS for each marketplace
    
    * chore: Remove unused imports
    
    * chore: Normalize LooksRare order signature's v byte
    
    * perf: Use hardhat to benchmark to avoid the extra gas
    
    * perf: Use hardhat to benchmark Gemswap
    
    * fix: Import from the wrong file
    
    * chore: Make buyer in hardhat test match foundry

--- test/foundry/LooksRareProxyTestHelpers.sol
+++ test/foundry/LooksRareProxyTestHelpers.sol
@@ -42,4 +42,42 @@ abstract contract LooksRareProxyTestHelpers {
         orders[1]
             .signature = hex"146a8f500fea9cde68c339da9abe8654ffb60c5a80506532e3500d1edba687640519093ff36ab3a728c961fc763d6e6a107ed823cfa5bd45182cab8029dab5d21b";
     }
+
+    function validBAYCId9483Order() internal pure returns (BasicOrder memory order) {
+        uint256[] memory amounts = new uint256[](1);
+        amounts[0] = 1;
+
+        order.signer = 0x17331428346E388f32013e6bEc0Aba29303857FD;
+        order.collection = BAYC;
+        order.collectionType = CollectionType.ERC721;
+        uint256[] memory tokenIds = new uint256[](1);
+        tokenIds[0] = 9483;
+        order.tokenIds = tokenIds;
+        order.amounts = amounts;
+        order.price = 73.88 ether;
+        order.currency = WETH;
+        order.startTime = 1662683712;
+        order.endTime = 1663288508;
+        order
+            .signature = hex"137b835f53750e28c2ca2c14686206fc1b5e44b47b22f619ae35ac7e661de1fa2d22c786fd897ad08ae30113d1a8804e2313b6410438ba9187e14e94ef25e4671c";
+    }
+
+    function validBAYCId3569Order() internal pure returns (BasicOrder memory order) {
+        uint256[] memory amounts = new uint256[](1);
+        amounts[0] = 1;
+
+        order.signer = 0x290dC64Bd6B079d0Fe41c8383D720938Bfa69cB1;
+        order.collection = BAYC;
+        order.collectionType = CollectionType.ERC721;
+        uint256[] memory tokenIds = new uint256[](1);
+        tokenIds[0] = 3569;
+        order.tokenIds = tokenIds;
+        order.amounts = amounts;
+        order.price = 74 ether;
+        order.currency = WETH;
+        order.startTime = 1662522412;
+        order.endTime = 1664367698;
+        order
+            .signature = hex"c0b72a9fc4068e489c0464b44c6e93ff28c24e30bb2ff1776d6afbc23d8235e75d4c996bf069c445133dcdc01dd1615fedfbb4a3e43fc539d5795215baf767101b";
+    }
 }
