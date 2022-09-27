428d8b483c28eef5a79112c08d7a02b4cd7ceff4
0xhiroshi <108636367+0xhiroshi@users.noreply.github.com>
2022-09-27 17:58:00 +0100

    refactor: Use Seaport.js interfaces (#46)

--- test/typescript/interfaces/seaport/offer.ts
+++
@@ -1,9 +0,0 @@
-import { BigNumber } from "ethers";
-
-export default interface Offer {
-  itemType: number;
-  token: string;
-  identifierOrCriteria: string;
-  startAmount: BigNumber;
-  endAmount: BigNumber;
-}
