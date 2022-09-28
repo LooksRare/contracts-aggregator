e64f8552b06b6975c7f3033aecbb6c3cd3044669
hiroshi <hiroshi@looksrare.org>
2022-09-28 09:29:46 +0100

    fix: Interface check should be external

--- contracts/proxies/V0LooksRareProxy.sol
+++ contracts/proxies/V0LooksRareProxy.sol
@@ -82,7 +82,7 @@ contract V0LooksRareProxy {
         uint256,
         uint256,
         bytes memory
-    ) public virtual returns (bytes4) {
+    ) external virtual returns (bytes4) {
         return this.onERC1155Received.selector;
     }
 
@@ -92,7 +92,7 @@ contract V0LooksRareProxy {
         uint256[] memory,
         uint256[] memory,
         bytes memory
-    ) public virtual returns (bytes4) {
+    ) external virtual returns (bytes4) {
         return this.onERC1155BatchReceived.selector;
     }
 }
