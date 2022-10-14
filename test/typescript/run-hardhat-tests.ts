c555de7f7098a5ea25118e87e10fa838bb8da1af
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2022-10-14 16:01:52 +0900

    chore: Adjustment tests

--- test/typescript/run-hardhat-tests.ts
+++ test/typescript/run-hardhat-tests.ts
@@ -1,6 +1,5 @@
 import { execSync } from "child_process";
 
-// TODO
 const config = [
   {
     fileName: "aggregator-sudoswap.ts",
@@ -9,6 +8,66 @@ const config = [
   {
     fileName: "aggregator-cryptopunks.ts",
     forkedBlock: 15358065,
+  },
+  {
+    fileName: "aggregator-x2y2.ts",
+    forkedBlock: 15346990,
+  },
+  {
+    fileName: "aggregator-looksrare.ts",
+    forkedBlock: 15282897,
+  },
+  {
+    fileName: "aggregator-seaport-erc721-atomic.ts",
+    forkedBlock: 15300884,
+  },
+  {
+    fileName: "aggregator-seaport-erc721-non-atomic.ts",
+    forkedBlock: 15300884,
+  },
+  {
+    fileName: "aggregator-seaport-erc721-only-usdc-orders-atomic.ts",
+    forkedBlock: 15491323,
+  },
+  {
+    fileName: "aggregator-seaport-erc721-only-usdc-orders-non-atomic.ts",
+    forkedBlock: 15491323,
+  },
+  {
+    fileName: "aggregator-seaport-erc1155-atomic.ts",
+    forkedBlock: 15320038,
+  },
+  {
+    fileName: "aggregator-seaport-erc1155-non-atomic.ts",
+    forkedBlock: 15320038,
+  },
+  {
+    fileName: "aggregator-seaport-multiple-collection-types.ts",
+    forkedBlock: 15323472,
+  },
+  {
+    fileName: "aggregator-seaport-multiple-currencies-atomic.ts",
+    forkedBlock: 15447813,
+  },
+  {
+    fileName: "aggregator-seaport-multiple-currencies-non-atomic.ts",
+    forkedBlock: 15447813,
+  },
+  {
+    fileName: "aggregator-seaport-erc721-multiple-currencies-random-order-fees-atomic.ts",
+    forkedBlock: 15491323,
+  },
+  {
+    fileName: "aggregator-seaport-erc721-multiple-currencies-random-order-fees-non-atomic.ts",
+    forkedBlock: 15491323,
+  },
+  {
+    fileName: "aggregator-conflicted-orders.ts",
+    forkedBlock: 15327113,
+  },
+  {
+    fileName: "aggregator-multiple-markets.ts",
+    forkedBlock: 15326566,
   },
 ];
 
