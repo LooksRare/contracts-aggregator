a298c75bae0f2f37874b8118ae15687b4308e471
hiroshi <hiroshi@looksrare.org>
2022-10-20 21:53:36 +0100

    perf: Cache order fee

--- contracts/proxies/SeaportProxy.sol
+++ contracts/proxies/SeaportProxy.sol
@@ -209,13 +209,14 @@ contract SeaportProxy is IProxy, TokenRescuer {
                 )
             {
                 if (feeRecipient != address(0)) {
+                    uint256 orderFee = (price * feeBp) / 10000;
                     if (currency == lastOrderCurrency) {
-                        fee += (price * feeBp) / 10000;
+                        fee += orderFee;
                     } else {
                         if (fee > 0) _transferFee(fee, lastOrderCurrency, feeRecipient);
 
                         lastOrderCurrency = currency;
-                        fee = (price * feeBp) / 10000;
+                        fee = orderFee;
                     }
                 }
             } catch {}
