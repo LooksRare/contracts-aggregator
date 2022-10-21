7222cfa547999a22fbce4576b712b4e359218abb
hiroshi <hiroshi@looksrare.org>
2022-10-21 16:07:29 +0100

    chore: Config can be array of string

--- test/typescript/run-hardhat-tests.ts
+++ test/typescript/run-hardhat-tests.ts
@@ -1,60 +1,28 @@
 import { execSync } from "child_process";
 
 const config = [
-  {
-    fileName: "aggregator-sudoswap.ts",
-  },
-  {
-    fileName: "aggregator-cryptopunks.ts",
-  },
-  {
-    fileName: "aggregator-x2y2.ts",
-  },
-  {
-    fileName: "aggregator-looksrare.ts",
-  },
-  {
-    fileName: "aggregator-seaport-erc721-atomic.ts",
-  },
-  {
-    fileName: "aggregator-seaport-erc721-non-atomic.ts",
-  },
-  {
-    fileName: "aggregator-seaport-erc721-only-usdc-orders-atomic.ts",
-  },
-  {
-    fileName: "aggregator-seaport-erc721-only-usdc-orders-non-atomic.ts",
-  },
-  {
-    fileName: "aggregator-seaport-erc1155.ts",
-  },
-  {
-    fileName: "aggregator-seaport-multiple-collection-types.ts",
-  },
-  {
-    fileName: "aggregator-seaport-multiple-currencies-atomic.ts",
-  },
-  {
-    fileName: "aggregator-seaport-multiple-currencies-non-atomic.ts",
-  },
-  {
-    fileName: "aggregator-seaport-erc721-multiple-currencies-random-order-fees-atomic.ts",
-  },
-  {
-    fileName: "aggregator-seaport-erc721-multiple-currencies-random-order-fees-non-atomic.ts",
-  },
-  {
-    fileName: "aggregator-conflicted-orders.ts",
-  },
-  {
-    fileName: "aggregator-multiple-markets.ts",
-  },
+  "aggregator-sudoswap.ts",
+  "aggregator-cryptopunks.ts",
+  "aggregator-x2y2.ts",
+  "aggregator-looksrare.ts",
+  "aggregator-seaport-erc721-atomic.ts",
+  "aggregator-seaport-erc721-non-atomic.ts",
+  "aggregator-seaport-erc721-only-usdc-orders-atomic.ts",
+  "aggregator-seaport-erc721-only-usdc-orders-non-atomic.ts",
+  "aggregator-seaport-erc1155.ts",
+  "aggregator-seaport-multiple-collection-types.ts",
+  "aggregator-seaport-multiple-currencies-atomic.ts",
+  "aggregator-seaport-multiple-currencies-non-atomic.ts",
+  "aggregator-seaport-erc721-multiple-currencies-random-order-fees-atomic.ts",
+  "aggregator-seaport-erc721-multiple-currencies-random-order-fees-non-atomic.ts",
+  "aggregator-conflicted-orders.ts",
+  "aggregator-multiple-markets.ts",
 ];
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function runHardhatTests(config: string | any[]): void {
   for (let i = 0; i < config.length; i++) {
-    execSync("npx hardhat test test/typescript/" + String(config[i].fileName), {
+    execSync("npx hardhat test test/typescript/" + String(config[i]), {
       stdio: "inherit",
     });
   }
