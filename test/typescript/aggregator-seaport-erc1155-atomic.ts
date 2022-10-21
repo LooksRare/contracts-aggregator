d4957e5093e049f3e297cc802000829957fa445b
hiroshi <hiroshi@looksrare.org>
2022-10-21 17:44:58 +0100

    Revert "chore: Combine ERC1155 atomic/non-atomic tests"
    This reverts commit 1f750c524b079b7f83afd9c42dcd326b560a8854.

---
+++ test/typescript/aggregator-seaport-erc1155-atomic.ts
@@ -0,0 +1,17 @@
+import { ethers } from "hardhat";
+import behavesLikeSeaportERC1155 from "./shared-tests/behaves-like-seaport-erc1155";
+
+describe("Aggregator", () => {
+  before(async () => {
+    await ethers.provider.send("hardhat_reset", [
+      {
+        forking: {
+          jsonRpcUrl: process.env.ETH_RPC_URL,
+          blockNumber: 15320038,
+        },
+      },
+    ]);
+  });
+
+  behavesLikeSeaportERC1155(true);
+});
