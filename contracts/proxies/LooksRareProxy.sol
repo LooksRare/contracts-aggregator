b887829cccca03fdf84ba5124e8e63405215df3f
hiroshi <hiroshi@looksrare.org>
2022-11-22 14:23:21 +0000

    perf: No need to set isOrderAsk to its default value

--- contracts/proxies/LooksRareProxy.sol
+++ contracts/proxies/LooksRareProxy.sol
@@ -89,7 +89,7 @@ contract LooksRareProxy is IProxy, TokenRescuer, TokenTransferrer, SignatureChec
 
             OrderTypes.TakerOrder memory takerBid;
             {
-                takerBid.isOrderAsk = false;
+                // No need to set isOrderAsk as its default value is false
                 takerBid.taker = address(this);
                 takerBid.price = order.price;
                 takerBid.tokenId = makerAsk.tokenId;
