4bdbe4af3a10ac8e5f9e88068365e0d0aa350112
hiroshi <hiroshi@looksrare.org>
2023-02-23 19:03:19 +0000

    test: ERC1155 test

--- test/foundry/LooksRareV2Proxy.t.sol
+++ test/foundry/LooksRareV2Proxy.t.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.17;
 
 import {IERC721} from "@looksrare/contracts-libs/contracts/interfaces/generic/IERC721.sol";
+import {IERC1155} from "@looksrare/contracts-libs/contracts/interfaces/generic/IERC1155.sol";
 import {LooksRareV2Proxy} from "../../contracts/proxies/LooksRareV2Proxy.sol";
 import {LooksRareAggregator} from "../../contracts/LooksRareAggregator.sol";
 import {ILooksRareAggregator} from "../../contracts/interfaces/ILooksRareAggregator.sol";
@@ -20,7 +21,7 @@ contract LooksRareV2ProxyTest is TestParameters, TestHelpers, LooksRareV2ProxyTe
     LooksRareV2Proxy private looksRareV2Proxy;
 
     function setUp() public {
-        vm.createSelectFork(vm.rpcUrl("goerli"), 8_542_878);
+        vm.createSelectFork(vm.rpcUrl("goerli"), 8_543_681);
 
         vm.deal(LOOKSRARE_V2_GOERLI, 1 wei);
 
@@ -29,18 +30,19 @@ contract LooksRareV2ProxyTest is TestParameters, TestHelpers, LooksRareV2ProxyTe
         aggregator.addFunction(address(looksRareV2Proxy), LooksRareV2Proxy.execute.selector);
 
         vm.deal(_buyer, 200 ether);
-        vm.deal(MULTIFACET_NFT_OWNER, 200 ether);
+        vm.deal(NFT_OWNER, 200 ether);
         vm.deal(address(aggregator), 1 wei);
 
-        vm.startPrank(MULTIFACET_NFT_OWNER);
+        vm.startPrank(NFT_OWNER);
         IERC721(MULTIFACET_NFT).setApprovalForAll(LOOKSRARE_V2_TRANSFER_MANAGER_GOERLI, true);
+        IERC1155(TEST_ERC1155).setApprovalForAll(LOOKSRARE_V2_TRANSFER_MANAGER_GOERLI, true);
         address[] memory operators = new address[](1);
         operators[0] = LOOKSRARE_V2_GOERLI;
         ITransferManager(LOOKSRARE_V2_TRANSFER_MANAGER_GOERLI).grantApprovals(operators);
         vm.stopPrank();
     }
 
-    function testExecute() public asPrankedUser(_buyer) {
+    function testExecuteERC721() public asPrankedUser(_buyer) {
         ILooksRareAggregator.TradeData[] memory tradeData = _generateTradeData();
         TokenTransfer[] memory tokenTransfers = new TokenTransfer[](0);
 
@@ -55,7 +57,23 @@ contract LooksRareV2ProxyTest is TestParameters, TestHelpers, LooksRareV2ProxyTe
         assertEq(IERC721(MULTIFACET_NFT).ownerOf(2828267), _buyer);
         assertEq(IERC721(MULTIFACET_NFT).ownerOf(2828268), _buyer);
         assertEq(_buyer.balance, 200 ether - value);
-        assertEq(address(MULTIFACET_NFT_OWNER).balance, 200 ether + (value * 9_800) / 10_000);
+        assertEq(address(NFT_OWNER).balance, 200 ether + (value * 9_800) / 10_000);
+    }
+
+    function testExecuteERC1155() public asPrankedUser(_buyer) {
+        ILooksRareAggregator.TradeData[] memory tradeData = _generateERC1155TradeData();
+        TokenTransfer[] memory tokenTransfers = new TokenTransfer[](0);
+
+        uint256 value = tradeData[0].orders[0].price + tradeData[0].orders[1].price;
+
+        vm.expectEmit(false, false, false, true);
+        emit Sweep(_buyer);
+        aggregator.execute{value: value}(tokenTransfers, tradeData, _buyer, _buyer, false);
+
+        assertEq(IERC1155(TEST_ERC1155).balanceOf(_buyer, 69), 5);
+        assertEq(IERC1155(TEST_ERC1155).balanceOf(_buyer, 420), 5);
+        assertEq(_buyer.balance, 200 ether - value);
+        assertEq(address(NFT_OWNER).balance, 200 ether + (value * 9_800) / 10_000);
     }
 
     function testExecuteCallerNotAggregator() public {
@@ -90,8 +108,8 @@ contract LooksRareV2ProxyTest is TestParameters, TestHelpers, LooksRareV2ProxyTe
         uint256 value = firstOrderPrice + tradeData[0].orders[1].price;
 
         // Seller no longer owns one of the NFTs in the second order
-        vm.prank(MULTIFACET_NFT_OWNER);
-        IERC721(MULTIFACET_NFT).transferFrom(MULTIFACET_NFT_OWNER, address(69), 2828267);
+        vm.prank(NFT_OWNER);
+        IERC721(MULTIFACET_NFT).transferFrom(NFT_OWNER, address(69), 2828267);
 
         vm.expectEmit(false, false, false, true);
         emit Sweep(_buyer);
@@ -101,7 +119,7 @@ contract LooksRareV2ProxyTest is TestParameters, TestHelpers, LooksRareV2ProxyTe
         assertEq(IERC721(MULTIFACET_NFT).balanceOf(_buyer), 1);
         assertEq(IERC721(MULTIFACET_NFT).ownerOf(2828266), _buyer);
         assertEq(_buyer.balance, 200 ether - firstOrderPrice);
-        assertEq(address(MULTIFACET_NFT_OWNER).balance, 200 ether + (firstOrderPrice * 9_800) / 10_000);
+        assertEq(address(NFT_OWNER).balance, 200 ether + (firstOrderPrice * 9_800) / 10_000);
     }
 
     function testExecuteRefundExtraPaid() public asPrankedUser(_buyer) {
@@ -119,7 +137,7 @@ contract LooksRareV2ProxyTest is TestParameters, TestHelpers, LooksRareV2ProxyTe
         assertEq(IERC721(MULTIFACET_NFT).ownerOf(2828267), _buyer);
         assertEq(IERC721(MULTIFACET_NFT).ownerOf(2828268), _buyer);
         assertEq(_buyer.balance, 200 ether - value);
-        assertEq(address(MULTIFACET_NFT_OWNER).balance, 200 ether + (value * 9_800) / 10_000);
+        assertEq(address(NFT_OWNER).balance, 200 ether + (value * 9_800) / 10_000);
     }
 
     function testExecuteZeroOrders() public asPrankedUser(_buyer) {
@@ -178,6 +196,47 @@ contract LooksRareV2ProxyTest is TestParameters, TestHelpers, LooksRareV2ProxyTe
 
     function _generateTradeData() private view returns (ILooksRareAggregator.TradeData[] memory tradeData) {
         BasicOrder[] memory orders = validGoerliTestERC721Orders();
+        MerkleTree memory merkleTree;
+
+        bytes[] memory ordersExtraData = new bytes[](2);
+        ordersExtraData[0] = abi.encode(
+            LooksRareV2Proxy.OrderExtraData({
+                merkleTree: merkleTree,
+                globalNonce: 0,
+                subsetNonce: 0,
+                orderNonce: 0,
+                strategyId: 0,
+                price: orders[0].price,
+                takerBidAdditionalParameters: new bytes(0),
+                makerAskAdditionalParameters: new bytes(0)
+            })
+        );
+
+        ordersExtraData[1] = abi.encode(
+            LooksRareV2Proxy.OrderExtraData({
+                merkleTree: merkleTree,
+                globalNonce: 0,
+                subsetNonce: 1,
+                orderNonce: 1,
+                strategyId: 0,
+                price: orders[1].price,
+                takerBidAdditionalParameters: new bytes(0),
+                makerAskAdditionalParameters: new bytes(0)
+            })
+        );
+
+        tradeData = new ILooksRareAggregator.TradeData[](1);
+        tradeData[0] = ILooksRareAggregator.TradeData({
+            proxy: address(looksRareV2Proxy),
+            selector: LooksRareV2Proxy.execute.selector,
+            orders: orders,
+            ordersExtraData: ordersExtraData,
+            extraData: abi.encode(address(0)) // affiliate
+        });
+    }
+
+    function _generateERC1155TradeData() private view returns (ILooksRareAggregator.TradeData[] memory tradeData) {
+        BasicOrder[] memory orders = validGoerliTestERC1155Orders();
         MerkleTree memory merkleTree;
 
         bytes[] memory ordersExtraData = new bytes[](2);
--- test/foundry/LooksRareV2ProxyTestHelpers.sol
+++ test/foundry/LooksRareV2ProxyTestHelpers.sol
@@ -8,8 +8,9 @@ import {CollectionType} from "../../contracts/libraries/OrderEnums.sol";
  * @notice LooksRare V2 orders helper contract
  */
 abstract contract LooksRareV2ProxyTestHelpers {
-    address internal constant MULTIFACET_NFT_OWNER = 0x7c741AD1dd7Ce77E88e7717De1cC20e3314b4F38;
+    address internal constant NFT_OWNER = 0x7c741AD1dd7Ce77E88e7717De1cC20e3314b4F38;
     address internal constant MULTIFACET_NFT = 0xf5de760f2e916647fd766B4AD9E85ff943cE3A2b;
+    address internal constant TEST_ERC1155 = 0x58c3c2547084CC1C94130D6fd750A3877c7Ca5D2;
 
     function validGoerliTestERC721Orders() internal pure returns (BasicOrder[] memory orders) {
         orders = new BasicOrder[](2);
@@ -17,7 +18,7 @@ abstract contract LooksRareV2ProxyTestHelpers {
         uint256[] memory amounts = new uint256[](1);
         amounts[0] = 1;
 
-        orders[0].signer = MULTIFACET_NFT_OWNER;
+        orders[0].signer = NFT_OWNER;
         orders[0].collection = MULTIFACET_NFT;
         orders[0].collectionType = CollectionType.ERC721;
         uint256[] memory tokenIds = new uint256[](1);
@@ -35,7 +36,7 @@ abstract contract LooksRareV2ProxyTestHelpers {
         amounts[0] = 1;
         amounts[1] = 1;
 
-        orders[1].signer = MULTIFACET_NFT_OWNER;
+        orders[1].signer = NFT_OWNER;
         orders[1].collection = MULTIFACET_NFT;
         orders[1].collectionType = CollectionType.ERC721;
         tokenIds = new uint256[](2);
@@ -49,5 +50,40 @@ abstract contract LooksRareV2ProxyTestHelpers {
         orders[1].endTime = 1679796047;
         orders[1]
             .signature = hex"412973e459a5816a99c0c91407cfc2ca901992a83f6ab477a468c47a90b742e632a01b11949e3d0076fc5481b604f8e1fa0e5e4c969f0ff781ac5afe0ebf41e41c";
+    }
+
+    function validGoerliTestERC1155Orders() internal pure returns (BasicOrder[] memory orders) {
+        orders = new BasicOrder[](2);
+
+        uint256[] memory amounts = new uint256[](1);
+        amounts[0] = 5;
+
+        orders[0].signer = NFT_OWNER;
+        orders[0].collection = TEST_ERC1155;
+        orders[0].collectionType = CollectionType.ERC1155;
+        uint256[] memory tokenIds = new uint256[](1);
+        tokenIds[0] = 69;
+        orders[0].tokenIds = tokenIds;
+        orders[0].amounts = amounts;
+        orders[0].price = 1 ether;
+        orders[0].currency = address(0);
+        orders[0].startTime = 1677166047;
+        orders[0].endTime = 1679796047;
+        orders[0]
+            .signature = hex"e85ab1c7f2e6ecf36aae078489195e86a4f9f4f14f24cb05776e154751e6da5f5d218cd3c1a3821a6613fd611185ac914a3da75861980c42ddeb20a0a6c244791b";
+
+        orders[1].signer = NFT_OWNER;
+        orders[1].collection = TEST_ERC1155;
+        orders[1].collectionType = CollectionType.ERC1155;
+        tokenIds = new uint256[](1);
+        tokenIds[0] = 420;
+        orders[1].tokenIds = tokenIds;
+        orders[1].amounts = amounts;
+        orders[1].price = 2 ether;
+        orders[1].currency = address(0);
+        orders[1].startTime = 1677166047;
+        orders[1].endTime = 1679796047;
+        orders[1]
+            .signature = hex"ad100b06a29212b754ffc782dfdcf755c7b13f4b061b41e94ae453526ec5ff737f02422cc11d464abc8e6c541cab04ea27de98ffe9eef5cb332d244aeecd95951c";
     }
 }
