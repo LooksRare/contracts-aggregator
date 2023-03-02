6361c2ccb580cfbb1afbae262a0eea7322a7dbf5
0xhiroshi <108636367+0xhiroshi@users.noreply.github.com>
2023-03-02 16:47:49 +0000

    chore: Remove LooksRare V2 from args (#173)

--- scripts/foundry/GoerliDeployment.s.sol
+++ scripts/foundry/GoerliDeployment.s.sol
@@ -5,13 +5,12 @@ import {Deployment} from "./Deployment.s.sol";
 
 contract GoerliDeployment is Deployment {
     address private constant LOOKSRARE = 0xD112466471b5438C1ca2D218694200e49d81D047;
-    address private constant LOOKSRARE_V2 = 0x35C2215F2FFe8917B06454eEEaba189877F200cf;
     address private constant SEAPORT = 0x00000000006c3852cbEf3e08E8dF289169EdE581;
 
     function run() public {
         if (block.chainid != 5) {
             revert WrongChain();
         }
-        _run(LOOKSRARE, LOOKSRARE_V2, SEAPORT);
+        _run(LOOKSRARE, SEAPORT);
     }
 }
