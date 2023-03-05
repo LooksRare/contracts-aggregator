a0228d2ea373b805f3b296b7546deb585544faa1
hiroshi <hiroshi@looksrare.org>
2023-03-05 21:22:08 +0000

    fix: Seaport 1.4 proxy deployment aggregator address is undefined

--- scripts/foundry/seaport-v1_4-deployment/Seaport_V1_4_ProxyDeployment.s.sol
+++ scripts/foundry/seaport-v1_4-deployment/Seaport_V1_4_ProxyDeployment.s.sol
@@ -10,6 +10,8 @@ contract Seaport_V1_4_ProxyDeployment is Script {
     IImmutableCreate2Factory private constant IMMUTABLE_CREATE2_FACTORY =
         IImmutableCreate2Factory(0x0000000000FFe8B47B3e2130213B802212439497);
 
+    address private constant LOOKS_RARE_AGGREGATOR = 0x00000000005228B791a99a61f36A130d50600106;
+
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
@@ -18,13 +20,11 @@ contract Seaport_V1_4_ProxyDeployment is Script {
             salt: vm.envBytes32("SEAPORT_V1_4_PROXY_SALT"),
             initializationCode: abi.encodePacked(
                 type(SeaportProxy).creationCode,
-                abi.encode(0x00000000000001ad428e4906aE43D8F9852d0dD6, address(looksRareAggregator))
+                abi.encode(0x00000000000001ad428e4906aE43D8F9852d0dD6, LOOKS_RARE_AGGREGATOR)
             )
         });
 
-        LooksRareAggregator looksRareAggregator = LooksRareAggregator(
-            payable(0x00000000005228B791a99a61f36A130d50600106)
-        );
+        LooksRareAggregator looksRareAggregator = LooksRareAggregator(payable(LOOKS_RARE_AGGREGATOR));
         looksRareAggregator.addFunction(seaportProxyAddress, SeaportProxy.execute.selector);
 
         vm.stopBroadcast();
