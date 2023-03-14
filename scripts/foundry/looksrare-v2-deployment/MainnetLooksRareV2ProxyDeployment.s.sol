25b6a4265960873b9c37b70da7e7094239913e72
Hiroshi <108636367+0xhiroshi@users.noreply.github.com>
2023-03-14 08:46:10 +0000

    build: Update LooksRare V2 address (#177)

--- scripts/foundry/looksrare-v2-deployment/MainnetLooksRareV2ProxyDeployment.s.sol
+++ scripts/foundry/looksrare-v2-deployment/MainnetLooksRareV2ProxyDeployment.s.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.17;
 import {LooksRareV2ProxyDeployment} from "./LooksRareV2ProxyDeployment.s.sol";
 
 contract MainnetLooksRareV2ProxyDeployment is LooksRareV2ProxyDeployment {
-    address private constant LOOKSRARE_V2 = 0x0000000000000000000000000000000000000000;
+    address private constant LOOKSRARE_V2 = 0x0000000000E655fAe4d56241588680F86E3b2377;
 
     function run() public {
         if (block.chainid != 1) {
